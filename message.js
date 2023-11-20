fe8e4fd7f6a481b31959f76dd5f2d5af0de9884d
wssommerhauser <130407147+wssommerhauser@users.noreply.github.com>
2023-11-19 22:09:47 -0600

    adding js files from desktop
    (git push isn't working from there)

--- message.js
+++ message.js
@@ -2,6 +2,9 @@ class Message {
    // Write code here!
    constructor(name, commands){
       this.name = name;
+      if (!name) {
+         throw Error("Message input required.");
+       }
       this.commands = commands;
    }
 }
--- rover.js
+++ rover.js
@@ -1,5 +1,41 @@
 class Rover {
-   // Write code here!
+   constructor(position){
+      this.position = position;
+      this.mode = "NORMAL";
+      this.generatorWatts = 110;
+   }
+   //message parameter will be a Message object, created from the Message Class.
+   receiveMessage(message){
+      let object = {
+         message: message.name,
+         results: []
+      };
+
+      for (let i = 0; i < message.commands.length; i++){
+         if (message.commands[i].commandType === "MOVE"){
+            if(this.mode = "LOW_POWER"){
+               object.results.push({completed: false});
+            }else{
+               object.results.push({completed: true});
+               this.position = message.commands[i].value;
+            }
+         } 
+         else if (message.commands[i].commandType === "STATUS_CHECK"){
+            object.results.push({
+               completed: true, 
+               roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position }
+            });
+         } 
+         else if (message.commands[i].commandType === "MODE_CHANGE"){
+            object.results.push({completed: true});
+            this.mode = message.commands[i].value;
+       } 
+      else{
+          object.results.push({completed: false});
+       }
+      return object;
+   }
+}
 }
 
 module.exports = Rover;
